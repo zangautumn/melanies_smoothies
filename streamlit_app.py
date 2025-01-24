21ced3604ac44e1da204f4493605a82f6e008508
zangautumn <46695192+zangautumn@users.noreply.github.com>
2025-01-23 23:32:31 -0600

    bo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ from snowflake.snowpark.functions import col
 
 cnx=st.connection('snowflake')
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options") .select(col("FRUIT_NAME"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options") .select(col("FRUIT_NAME"),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
